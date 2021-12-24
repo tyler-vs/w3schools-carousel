f306d147ba958257ea5c7c2444e37facca8755a0
tyler-vs <tylervanschaick@gmail.com>
2021-12-23 19:32:01 -0500

    Revert "Feature: JSON carousel #1"
    This reverts commit b18894dd16d48b57e2c77b6b4747104c6a11800a.

--- README.md
+++ README.md
@@ -2,13 +2,3 @@
 w3school carousel
 
 See how to build a [JavaScript carousel here](https://www.w3schools.com/howto/howto_js_slideshow.asp)
-
-Run locally with `python -m SimpleHTTPServer 4000`.
-
-Inspirations:
-
-- [BXSlider](https://bxslider.com/install/)
-- [Flickity](https://flickity.metafizzy.co/)
-- [Bootstrap Carousel](https://getbootstrap.com/docs/5.1/components/carousel/#via-javascript)
-
-https://codepen.io/tvs/pen/77d3abe973ed4c9f89caf373a5e38b6d?editors=1111
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+/**
+ * Carousel class
+ * @author Tyler Van Schaick
+ * @file Carousel class
+ */
 class Carousel {
   constructor(selector) {
-    // @TODO Feature detection
 
-    if (!selector) {
-      throw new InvalidSelectorError("A selector is required!");
-    }
+    // @TODO Feature detection
 
     // Get DOM elements
     this.carousel = document.querySelector(selector);
@@ -12,30 +14,17 @@ class Carousel {
       console.error("Error, no carousel element found! Bail!");
       return false;
     }
-    // Bind "this" to methods
-    this.init = this.init.bind(this);
-    this.init();
-  }
-
-  /**
-   * Initializes the carousel by:
-   * - setting up variables
-   * - Getting data- attribute values
-   * - Binding "this" to methods
-   * - Invoke addEventListeners() method
-   * - Show initial slide
-   */
-  init() {
-    // Set up variables
-    this.slideIndex = null;
-    this.sliderInterval = null;
-    this.slides = this.carousel.querySelectorAll(".carousel-item");
     this.playButton = this.carousel.querySelector(".button--play");
     this.nextButton = this.carousel.querySelector(".button--next");
     this.prevButton = this.carousel.querySelector(".button--prev");
     this.pauseButton = this.carousel.querySelector(".button--pause");
 
+    // Set variables
+    this.slideIndex = null;
+    this.sliderInterval = null;
+
     // Get data- attribute values
+    // Last image goes back to first image
     this.wrap = this.carousel.hasAttribute("data-wrap")
       ? // Convert the boolean string data- attribute to a real boolean value using this technique https://stackoverflow.com/a/264037
         this.carousel.getAttribute("data-wrap") === "true"
@@ -45,383 +34,125 @@ class Carousel {
       : defaults.interval;
     this.slideIndex = 1;
 
-    // Bind "this" with methods
+    // Bind functions
     this.play = this.play.bind(this);
     this.nextSlide = this.nextSlide.bind(this);
     this.prevSlide = this.prevSlide.bind(this);
     this.pause = this.pause.bind(this);
+    this.showDivs = this.showDivs.bind(this);
 
-    // Even private methods
-    this.showSlide = this.showSlide.bind(this);
-
-    // Add event listeners
-    this.addEventListeners();
+    this.addEventListenersV2();
 
     // Init
-    this.showSlide(this.slideIndex);
-  }
+    this.showDivs(this.slideIndex);
+  } // end constructor
 
-  // Binding in the constructor instead of here.
-  addEventListeners() {
+  /**
+   * Add carousel related event listeners.
+   */
+  addEventListenersV2() {
+    // Add event listeners
     this.playButton.addEventListener("click", this.play);
     this.nextButton.addEventListener("click", this.nextSlide);
     this.prevButton.addEventListener("click", this.prevSlide);
     this.pauseButton.addEventListener("click", this.pause);
   }
 
-  showSlide(n) {
+  /**
+   * Show a specified div element/carousel slide
+   * @param  {number} n carousel slide index number
+   * @return {[type]}   [description]
+   */
+  showDivs(n) {
     var i;
+    var x = this.carousel.querySelectorAll(".carousel-item");
 
     // Go back to first slide
-    if (n > this.slides.length) {
+    if (n > x.length) {
       if (this.wrap == true) {
         this.slideIndex = 1;
       } else {
-        this.slideIndex = this.slides.length;
+        this.slideIndex = x.length;
         return;
       }
     }
     // Go to last slide
     if (n < 1) {
       if (this.wrap == true) {
-        this.slideIndex = this.slides.length;
+        this.slideIndex = x.length;
       } else {
         this.slideIndex = 1;
         return;
       }
     }
-    for (i = 0; i < this.slides.length; i++) {
-      this.slides[i].style.display = "none";
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
     }
-    this.slides[this.slideIndex - 1].style.display = "block";
-  }
+    x[this.slideIndex - 1].style.display = "block";
+  };
+
+  /**
+   * Pause the carousel
+   * @return {[type]} [description]
+   */
   pause() {
     clearInterval(this.sliderInterval);
-  }
+  };
+
+  /**
+   * Go to previous carousel slide
+   * @return {[type]} [description]
+   */
   prevSlide() {
-    this.showSlide((this.slideIndex += -1));
-  }
+    this.showDivs((this.slideIndex += -1));
+  };
+
+  /**
+   * Go to next carousel slide
+   * @return {[type]} [description]
+   */
   nextSlide() {
-    this.showSlide((this.slideIndex += 1));
-  }
+    this.showDivs((this.slideIndex += 1));
+  };
+
+  /**
+   * Play the carousel slides
+   * @return {[type]} [description]
+   */
   play() {
     this.sliderInterval = setInterval(() => {
       this.nextSlide();
     }, this.interval);
-  }
-}
+  };
+};
 
-// Sub classing with "extends" on the Carousel class.
-// This Super JSON Carousel will use JSON to generate a similar
-// carousel slider like the Carousel class does.
-class SuperJSONCarousel extends Carousel {
-  constructor(selector, slidesObject) {
-    // Pass selector to the parent class
+// Initiate
+const carousel1 = new Carousel("#carousel--1");
+
+// Extend SuperCarousel from the Carousel class
+class SuperCarousel extends Carousel {
+  constructor(selector) {
     super(selector);
-
-    // Get slides object/array
-    this.slides = slidesObject;
-
-    // Add auto controls (play/pause) support
-    this.autoControls = this.carousel.hasAttribute("data-auto-controls")
-      ? this.carousel.getAttribute("data-auto-controls") === "true"
-      : false;
-    console.log("this.autoControls", this.autoControls);
-    if (this.autoControls) {
-      this.renderAutoControls = this.renderAutoControls.bind(this);
-    }
-    // Add fade support
-    this.fade = this.carousel.hasAttribute("data-fade")
-      ? this.carousel.getAttribute("data-fade") === "true"
-      : false;
-    console.log("this.fade", this.fade);
-    // Add caption support
-    this.captions = this.carousel.hasAttribute("data-captions")
-      ? this.carousel.getAttribute("data-captions") === "true"
-      : false;
-    console.log("this.captions", this.captions);
-    if (this.captions) {
-      this.renderCaptions = this.renderCaptions.bind(this);
-    }
-    // Add pager support
-    this.pager = this.carousel.hasAttribute("data-pager")
-      ? this.carousel.getAttribute("data-pager") === "true"
-      : false;
-    console.log("this.pager", this.pager);
-    if (this.pager) {
-      this.renderPager = this.renderPager.bind(this);
-    }
-
-    // Bind methods with "this"
-    this.renderApp = this.renderApp.bind(this);
-    this.renderSlide = this.renderSlide.bind(this);
-    this.renderButtons = this.renderButtons.bind(this);
-
-    // Render the slides
-    this.carousel.innerHTML = this.renderApp(this.slides);
-
-    // Call super/parent init method
-    super.init();
-
-    // Must be called after super.init();
-    // Get "slide to" buttons
     this.slideToButtons = this.carousel.querySelectorAll("[data-slide-to]");
-    // Bail if no "slide to" buttons found
     if (!this.slideToButtons.length > 0) {
-      console.log('Error, no "slide-to" buttons found.');
+      console.error('Error, no "slide-to" buttons found.');
       return false;
     }
-    // Loop through each button
     this.slideToButtons.forEach((button) => {
-      // Add click event handler
       button.addEventListener("click", (event) => {
-        // Select slide
         this.selectSlide(Number(event.target.dataset.slideTo));
       });
     });
-  }
-
-  // Add an init() method that basically overrides the parent classes
-  // init() method. This is because the parent init() method assumes
-  // DOM elements will be available, while in this extended class
-  // we are building the DOM elements in the script.
-  //
-  // We need to render those script created elements before
-  // running the parent init() method.
-  /**
-   * Init to override the original classe's init method
-   */
-  init() {
-    console.count("SuperJSONCarousel init");
-  }
+  }; // end constructor
 
   /**
-   * Returns an UL with rendered slides
-   * @param  {Array} slides Array of slide data.
-   */
-  renderApp(slides) {
-    return [
-      "<div class='carousel-items'>",
-      slides
-        .map((heading, i) => {
-          return this.renderSlide(heading, i);
-        })
-        .join(""),
-      this.renderSlide(),
-      "</div>",
-      this.renderButtons()
-    ].join("");
-  }
-
-  /**
-   * Render a slide's HTML
-   * @param  {Object} slide Slide object
-   * @param  {Number} index Index number
-   */
-  renderSlide(slide, index) {
-    if (!slide) {
-      console.log("Error. Must provide slide parameter");
-      return;
-    }
-    return [
-      "<div class='carousel-item carousel-item--" +
-        index +
-        " " +
-        (this.fade === true ? "fade" : "") +
-        " '>",
-      "<div class='carousel-item__inner'>",
-      "<div class='carousel-item__box-ratio'>",
-      "<img src='" + slide.image + "' class='carousel-item__img'>",
-      "</div>",
-      this.renderCaptions(slide),
-      "</div>",
-      "</div>"
-    ].join("");
-  }
-  /**
-   * Render pager
-   */
-  renderPager() {
-    if (!this.pager) {
-      console.log("Error. Pager is not set to true.");
-      return;
-    }
-
-    // Old school way
-    return [
-      "<button class='button button--indicator' data-slide-to='1'>Slide to #1</button>",
-      "<button class='button button--indicator' data-slide-to='2'>Slide to #2</button>",
-      "<button class='button button--indicator' data-slide-to='3'>Slide to #3</button>",
-      "<button class='button button--indicator' data-slide-to='4'>Slide to #4 (nope)</button>",
-      "<button class='button button--indicator' data-slide-to='5'>Slide to #5 (nope)</button>"
-    ].join("");
-
-    // Get an array of DOM strings
-    const arr = [];
-
-    // Check that we have slides
-    if (this.slidesLength > 0) {
-
-      // For each slide
-      this.slides.forEach((slide, i) => {
-        // Push render dot into the array
-        arr.push(this.renderDot(i + 1));
-      });
-
-    } else {
-      console.log("Error. No slides.");
-      return;
-    }
-
-    return arr.join("");
-  }
-
-  renderDot(i) {
-    return [
-      "<button class='button button--indicator' data-slide-to='" + i + "'>",
-      "Slide to #" + i + ".",
-      "</button>"
-    ].join("");
-  }
-
-  /**
-   * Render slide captions
-   * @param  {Object} slide Slide object
-   */
-  renderCaptions(slide) {
-    if (!slide) {
-      console.log("No slide data for captions passed.");
-      return false;
-    }
-    return [
-      "<div class='carousel-item__caption'>",
-      "<h3>" + slide.heading + "</h3>",
-      "<p>" + slide.subheading + "</p>",
-      "</div>"
-    ].join("");
-  }
-
-  /**
-   * Helper method to create HTML button elements from scratch.
-   * @param  {String} content HTML/string content
-   * @param  {String} css     String for CSS classes
-   * @param  {String} title   String title for title attribute
-   * @param  {String} data    String value for data- attribute
-   */
-  renderButton(args) {
-
-    let {content, css, title, data} = args;
-
-    if (!content) {
-      console.log("Must provide content to the renderButton function.");
-      return;
-    }
-
-    return [
-      "<button ",
-      this.renderClassAttribute(css),
-      this.renderDataAttribute("slide", data),
-      this.renderTitleAttribute(title),
-      ">",
-      content,
-      "</button>"
-    ].join("");
-  }
-
-  // @todo handle [...class] array of classes
-  /**
-   * Helper method to render class attribute.
-   * @param  {String} name Class attribute value
-   */
-  renderClassAttribute(name) {
-    if (!name) {
-      console.log("Error. Must provide a class name.");
-      return;
-    }
-    return `class="${name}" `;
-  }
-
-  /**
-   * Helper method for rendering title attribute.
-   * @param  {String} title Title attirbute value.
-   */
-  renderTitleAttribute(title) {
-    if (!title) {
-      console.log("Error. Must provide a title for the title attribute.");
-      return;
-    }
-    // return 'title="' + title + '" ';
-    return `title="${title}" `;
-  }
-
-  /**
-   * Helper method for rendering a data- attribute.
-   * @param  {String} name  Value that comes after data-
-   * @param  {String} value String value
-   */
-  renderDataAttribute(name = "slide", value) {
-    if (!value) {
-      console.log(
-        "Error. Value must be provided for the renderDataAttribute function."
-      );
-      return;
-    }
-    return `data-${name}="${value}"`;
-  }
-
-  /**
-   * Render auto controls (play/pause) buttons
-   */
-  renderAutoControls() {
-    if (!this.autoControls) {
-      return "";
-    }
-    // Set up button options as objects.
-    const playBtnOptions = {
-      content: "Play",
-      css: "button button--play"
-    }
-    const pauseBtnOptions = {
-      content: "Pause",
-      css: "button button--pause"
-    }
-    return [
-      this.renderButton(playBtnOptions),
-      this.renderButton(pauseBtnOptions)
-    ].join("");
-  }
-
-  /**
-   * Helper method to render slideshow buttons.
-   */
-  renderButtons() {
-    const prevBtnOptions = {
-      content: "&#10094;",
-      css: "button button--prev",
-      title: "Prev",
-      data: "prev"
-    }
-    const nextBtnOptions = {
-      content: "&#10095;",
-      css: "button button--next",
-      title: "Next",
-      data: "next"
-    }
-    return [
-      this.renderPager(),
-      this.renderButton(prevBtnOptions),
-      this.renderAutoControls(),
-      this.renderButton(nextBtnOptions)
-    ].join("");
-  }
-
-  /**
-   * Select slide by number
+   * Select a certain carousel slide
    * @param  {[type]} n [description]
+   * @return {[type]}   [description]
    */
   selectSlide(n) {
-    this.showSlide((this.slideIndex = n));
-  }
-}
+    this.showDivs((this.slideIndex = n));
+  };
+};
 
-// Static methods, logger utility, utility functions, mixins
+const carousel2 = new SuperCarousel("#carousel--2");
